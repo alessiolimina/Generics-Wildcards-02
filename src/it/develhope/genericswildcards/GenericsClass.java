19b6e88da6ad8eb07eca08b755d0c706bfd3c964
Alessio Limina <alessio.limina@gmail.com>
2023-02-03 19:28:51 +0100

    Exercise: generics-wildcards-2

--- src/it/develhope/genericswildcards/GenericsClass.java
+++ src/it/develhope/genericswildcards/GenericsClass.java
@@ -4,19 +4,16 @@ public class GenericsClass <T> {
     private T t;
 
     public void setT(T valueToSet) {
-        this.t = valueToSet;
-    }
+        this.t = valueToSet;}
 
-    public T getT() {
-        return t;
-    }
+    public T getT() {return t;}
 
     public static <T> void isEqual(GenericsClass gc1, GenericsClass gc2) {
         boolean b = gc1.getT().equals(gc2.getT());
         if (b) {System.out.println("True");
         } else {System.out.println("False");}
+      }
     }
-}
 
 
 
