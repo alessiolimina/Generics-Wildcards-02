558ff0a476867a8df2248bf50e8bcda600af84d2
Alessio Limina <alessio.limina@gmail.com>
2023-02-03 19:25:46 +0100

    Exercise: generics-wildcards-2

--- src/it/develhope/genericswildcards/Start.java
+++ src/it/develhope/genericswildcards/Start.java
@@ -2,30 +2,34 @@ package it.develhope.genericswildcards;
 
 public class Start {
     public static void main(String[] args) {
+
+        System.out.println("-------------------Creating classes------------------");
+
         GenericsClass <Integer> firstElement = new GenericsClass<>();
         GenericsClass <Integer> secondElement = new GenericsClass<>();
         GenericsClass <String> thirdElement = new GenericsClass<>();
         GenericsClass <String> fourthElement = new GenericsClass<>();
+
         firstElement.setT(12);
         secondElement.setT(13);
         thirdElement.setT("This is a string");
         fourthElement.setT("This is a string");
-        System.out.println("Is the first element equal to the second element? " + );
+
+        System.out.println("First element: " + firstElement.getT());
+        System.out.println("Second element: " + secondElement.getT());
+        System.out.println("Third Element: " + thirdElement.getT());
+        System.out.println("Fourth element: " + fourthElement.getT());
+
+        System.out.println("---------------Comparing elements---------------------");
+
+        System.out.print("Is the first element equal to the second element? ");
+        GenericsClass.isEqual(firstElement, secondElement);
+        System.out.print("Is the third element equal to the fourth element? ");
+        GenericsClass.isEqual(thirdElement, fourthElement);
+
+        System.out.println("-------------------------------------------------------");
 
     }
 }
 
-//Todo input con scanner and notify the user
-/*
 
-create 4 new GenericsClass objects where T is:
-Integer for firstElement and secondElement
-String for thirdElement and fourthElement
-assign the following object-value using the setT() method:
-firstElement will have an int value 12
-secondElement will have an int value 13
-thirdElement and fourthElement will have the same String value "This is a string"
-call the static method isEqual() for answering the following two comparisons (and notify the user):
-first element is equal to second element?
-third element is equal to fourth element?
- */
